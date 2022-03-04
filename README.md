345c9230ca6de935e96a71ff33f10d3233474c64
lishu <lishu@gaodun.com>
2022-03-04 10:13:02 +0800

    add

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ typeOf({}); // 'object'
 typeOf(new Date()); // 'date'
 ```
 
-## 继承
+<!-- ## 继承
 
 ### 原型链继承
 
@@ -29,4 +29,33 @@ let dog1 = new Dog();
 dog1.colors.push("brown");
 let dog2 = new Dog();
 console.log(dog2.colors);
+``` -->
+
+## 数组去重
+
+```js
+// Es5
+function unique(arr) {
+  let newArr = arr.filter((item, index, array) => {
+    // array: 调用filter的对象：arr
+    // filter 函数为判断条件
+    // array.indexOf(item)!==-1    X
+    return array.indexOf(item) === index;
+  });
+  return newArr;
+}
+// es6
+const unique = (arr) => [...new Set(arr)];
 ```
+<!-- 
+## 数组扁平化
+
+```js
+const flat = (arr)=>{
+  let result = []
+
+}
+[1, [2, [3]]].flat(2)  // [1, 2, 3]
+
+
+``` -->
--- main.js
+++ main.js
@@ -1,1 +1,70 @@
-// 
+// 类型检测
+function typeOf(obj) {
+  // 评论区里提到的更好的写法
+  return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+}
+typeOf([]); // 'array'
+typeOf({}); // 'object'
+typeOf(new Date()); // 'date'
+
+// 原型链继承
+function Animal() {
+  this.colors = ["black", "white"];
+}
+Animal.prototype.getColor = function () {
+  return this.colors;
+};
+function Dog() {}
+Dog.prototype = new Animal();
+
+let dog1 = new Dog();
+dog1.colors.push("brown");
+let dog2 = new Dog();
+console.log(dog2.colors);
+
+// 构造函数继承
+function Animal2(name) {
+  this.name = name;
+  this.getName = function () {
+    return this.name;
+  };
+}
+function Dog2(name) {
+  Animal.call(this, name);
+}
+Dog.prototype = new Animal2();
+
+// 组合继承
+
+// 寄生式组合继承
+
+// class 继承
+class Animal5 {
+  constructor(name) {
+    this.name = name;
+  }
+  getName() {
+    return this.name;
+  }
+}
+
+class Dog extends Animal5 {
+  constructor(name, age) {
+    super(name);
+    this.age = age;
+  }
+}
+
+// 数组去重
+// Es5
+function unique(arr){
+  let newArr = arr.filter((item,index,array)=>{
+    // array: 调用filter的对象：arr
+    // filter 函数为判断条件
+    // array.indexOf(item)!==-1    X
+    return array.indexOf(item) === index
+  })
+  return newArr
+}
+// es6
+const unique = arr=>[...new Set(arr)]
